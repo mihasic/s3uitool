d8bd3e2a0c430c5fac1299e2272ad59fdc031064
Mike Rybnikov <m.rybnikov@fugro.com>
2025-12-29 13:35:58 +0100

    better timeout for e2e

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 10 * 1000,
+  },
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
@@ -21,7 +25,7 @@ export default defineConfig({
     command: 'cd .. && bun run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
-    stdout: 'ignore',
+    stdout: 'pipe',
     stderr: 'pipe',
     timeout: 120 * 1000,
   },
